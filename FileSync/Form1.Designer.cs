228e7e26c257e9d7a904a05b170562542a9f73d4
olesbober <ragekage23424@gmail.com>
2021-07-07 18:07:44 -0700

    added Buttons and FolderBrowserDialogs

--- FileSync/Form1.Designer.cs
+++ FileSync/Form1.Designer.cs
@@ -30,22 +30,95 @@ namespace FileSync
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.folder1button = new System.Windows.Forms.Button();
+            this.folder1label = new System.Windows.Forms.Label();
+            this.folder2button = new System.Windows.Forms.Button();
+            this.folder2label = new System.Windows.Forms.Label();
+            this.syncbutton = new System.Windows.Forms.Button();
+            this.syncnowlabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // folder1button
+            // 
+            this.folder1button.Location = new System.Drawing.Point(12, 12);
+            this.folder1button.Name = "folder1button";
+            this.folder1button.Size = new System.Drawing.Size(133, 39);
+            this.folder1button.TabIndex = 0;
+            this.folder1button.Text = "Pick Folder 1";
+            this.folder1button.UseVisualStyleBackColor = true;
+            this.folder1button.Click += new System.EventHandler(this.folder1button_Click);
+            // 
+            // folder1label
+            // 
+            this.folder1label.AutoSize = true;
+            this.folder1label.Location = new System.Drawing.Point(151, 19);
+            this.folder1label.Name = "folder1label";
+            this.folder1label.Size = new System.Drawing.Size(0, 25);
+            this.folder1label.TabIndex = 1;
+            // 
+            // folder2button
+            // 
+            this.folder2button.Location = new System.Drawing.Point(12, 57);
+            this.folder2button.Name = "folder2button";
+            this.folder2button.Size = new System.Drawing.Size(133, 34);
+            this.folder2button.TabIndex = 2;
+            this.folder2button.Text = "Pick Folder 2";
+            this.folder2button.UseVisualStyleBackColor = true;
+            this.folder2button.Click += new System.EventHandler(this.folder2button_Click);
+            // 
+            // folder2label
+            // 
+            this.folder2label.AutoSize = true;
+            this.folder2label.Location = new System.Drawing.Point(151, 62);
+            this.folder2label.Name = "folder2label";
+            this.folder2label.Size = new System.Drawing.Size(0, 25);
+            this.folder2label.TabIndex = 1;
+            // 
+            // syncbutton
+            // 
+            this.syncbutton.Location = new System.Drawing.Point(12, 404);
+            this.syncbutton.Name = "syncbutton";
+            this.syncbutton.Size = new System.Drawing.Size(112, 34);
+            this.syncbutton.TabIndex = 3;
+            this.syncbutton.Text = "Sync Now";
+            this.syncbutton.UseVisualStyleBackColor = true;
+            this.syncbutton.Click += new System.EventHandler(this.syncbutton_Click);
+            // 
+            // syncnowlabel
+            // 
+            this.syncnowlabel.AutoSize = true;
+            this.syncnowlabel.Location = new System.Drawing.Point(130, 409);
+            this.syncnowlabel.Name = "syncnowlabel";
+            this.syncnowlabel.Size = new System.Drawing.Size(0, 25);
+            this.syncnowlabel.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.syncnowlabel);
+            this.Controls.Add(this.syncbutton);
+            this.Controls.Add(this.folder2label);
+            this.Controls.Add(this.folder2button);
+            this.Controls.Add(this.folder1label);
+            this.Controls.Add(this.folder1button);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FileSync";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button folder1button;
+        private System.Windows.Forms.Label folder1label;
+        private System.Windows.Forms.Button folder2button;
+        private System.Windows.Forms.Label folder2label;
+        private System.Windows.Forms.Button syncbutton;
+        private System.Windows.Forms.Label syncnowlabel;
     }
 }
-
--- FileSync/Form1.cs
+++ FileSync/Form1.cs
@@ -12,10 +12,54 @@ namespace FileSync
 {
     public partial class Form1 : Form
     {
+
+        public bool folder1Selected = false;
+        public bool folder2Selected = false;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void folder1button_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd1 = new FolderBrowserDialog();
+            if (fbd1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                folder1label.Text = "Folder 1 is " + fbd1.SelectedPath;
+                folder1Selected = true;
+            }
+            else
+            {
+                folder1label.Text = "Something bad happened.";
+                folder1Selected = false;
+            }
+        }
+
+        private void folder2button_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd2 = new FolderBrowserDialog();
+            if (fbd2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                folder2label.Text = "Folder 2 is " + fbd2.SelectedPath;
+                folder2Selected = true;
+            }
+            else
+            {
+                folder2label.Text = "Something bad happened.";
+                folder2Selected = false;
+            }
+        }
+
+        private void syncbutton_Click(object sender, EventArgs e)
+        {
+            if (!folder1Selected || !folder2Selected)
+            {
+                syncnowlabel.Text = "One of the folders has not been properly selected.";
+            }
+            else
+            {
+                syncnowlabel.Text = "This should now sync the contents of the two folders.";
+            }
+        }
     }
 }
